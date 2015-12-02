edd7480cf0642a4169a76fbc413158ee01f87694
Chad Mitchell <chad39@gmail.com>
2015-12-02 12:07:06 -0700

    Create htb.js

---
+++ htb.js
@@ -0,0 +1,12 @@
+// JavaScript implementation of the head/tail breaks algorithm.
+// see >> http://arxiv.org/ftp/arxiv/papers/1209/1209.2801.pdf
+
+function htb(data) {
+   var data_mean = data.reduce(function(a, b){return a + b})/data.length;
+   var head = data.filter(function(d){return d > data_mean});
+	 var tail = data.filter(function(d){return d < data_mean});
+	 console.log(data_mean);
+	 while (head.length > 1) {
+	    return htb(head);
+	 };
+}
