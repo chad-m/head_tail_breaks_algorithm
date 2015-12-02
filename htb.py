803a20d127256092a7944c4816919122b84a0e88
Chad Mitchell <chad39@gmail.com>
2015-12-02 12:04:31 -0700

    Update htb.py

--- htb.py
+++ htb.py
@@ -1,7 +1,7 @@
 # python implementation of the head/tail breaks algorithm 
 # see >> http://arxiv.org/ftp/arxiv/papers/1209/1209.2801.pdf
 def htb(data):
-    data_mean = np.mean(data)
+    data_mean = sum(data)/float(len(data))
     head = [i for i in data if i > data_mean]
     tail = [i for i in data if i < data_mean]
     print data_mean 
