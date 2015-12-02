edd28655f0453e1e77a11825de5d50ed3737e69b
Chad Mitchell <chad39@gmail.com>
2015-12-02 12:10:22 -0700

    Update htb.js

--- htb.js
+++ htb.js
@@ -2,11 +2,11 @@
 // see >> http://arxiv.org/ftp/arxiv/papers/1209/1209.2801.pdf
 
 function htb(data) {
-   var data_mean = data.reduce(function(a, b){return a + b})/data.length;
-   var head = data.filter(function(d){return d > data_mean});
-	 var tail = data.filter(function(d){return d < data_mean});
-	 console.log(data_mean);
-	 while (head.length > 1) {
-	    return htb(head);
-	 };
+    var data_mean = data.reduce(function(a, b){return a + b})/data.length;
+    var head = data.filter(function(d){return d > data_mean});
+    var tail = data.filter(function(d){return d < data_mean});
+    console.log(data_mean);
+    while (head.length > 1) {
+        return htb(head);
+    };
 }
